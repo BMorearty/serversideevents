ad29f5a3763caa20024a4acc47620373c74667a1
Brian Morearty <brian@morearty.org>
2020-06-26 12:34:17 -0700

    initial

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+var app = express()
+
+// Page handler
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
